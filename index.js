c0c3999f158517704723b6b9aca426fe9793f37b
Troy McCabe <troymccabe@gmail.com>
2018-02-01 00:13:38 -0600

    Adds blockexplorer.com for btc & bch

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function getDonationsFromCryptoID(currency, addressId, notify, intervalSeconds) 
                 */
                 var resp = JSON.parse(json);
                 var timeBetweenRuns = Math.floor(Date.now() / 1000) - intervalSeconds;
-                var txTimestamp, msg;
+                var txTimestamp;
 
                 for (var i = 0; i < resp.tx.length; i++) {
                     // if it's the first transaction, it's got the full timestamp
@@ -54,9 +54,10 @@ function getDonationsFromCryptoID(currency, addressId, notify, intervalSeconds) 
                     // the transaction occurred between the last run and now
                     if (txTimestamp > timeBetweenRuns) {
                         notify(
-                            `New ${currency.toUpperCase()} donation of ${resp.tx[i][4]} ${currency.toUpperCase()}! ` +
-                            `The wallet now has a balance of ${resp.tx[i][5]} ${currency.toUpperCase()}.` +
-                            `\nView the tx here: https://chainz.cryptoid.info/${currency}/tx.dws?${resp.tx[i][0]}.htm`, 
+                            currency.toUpperCase(), 
+                            resp.tx[i][4], 
+                            resp.tx[i][5],
+                            `https://chainz.cryptoid.info/${currency}/tx.dws?${resp.tx[i][0]}.htm`,
                             i
                         );
                     }
@@ -67,31 +68,96 @@ function getDonationsFromCryptoID(currency, addressId, notify, intervalSeconds) 
         });
 }
 
+ /**
+  * Gets transactions within the time period from CryptoID
+  */
+function getDonationsFromBlockExplorer(currency, address, notify, intervalSeconds) {
+    var domain = `${currency == 'bch' ? 'bitcoincash.' : ''}blockexplorer.com`;
+    var https = require('https');
+    https.get(
+        `https://${domain}/api/addrs/${address}/txs`, 
+        (res) => {
+            var json = '';
+            res.on('data', function(chunk) {
+                json += chunk;
+            });
+            res.on('end', function() {
+                var resp = JSON.parse(json);
+                var timeBetweenRuns = Math.floor(Date.now() / 1000) - intervalSeconds;
+                var tx, vout;
+
+                tx:
+                for (var i = 0; i < resp.items.length; i++) {
+                    tx = resp.items[i];
+
+                    // the transaction occurred between the last run and now
+                    if (tx.time > timeBetweenRuns) {
+                        for (var j = 0; j < tx.vout.length; j++) {
+                            vout = tx.vout[j];
+
+                            for (var k = 0; k < vout.scriptPubKey.addresses.length; k++) {
+                                if (vout.scriptPubKey.addresses[k] == address) {
+                                    https.get(
+                                        `https://${domain}/api/addr/${address}/balance`, 
+                                        (res) => {
+                                            var balance = '';
+                                            res.on('data', function(chunk) {
+                                                balance += chunk;
+                                            });
+                                            res.on('end', function() {
+                                                notify(
+                                                    currency.toUpperCase(),
+                                                    vout.value,
+                                                    balance * 1e-8,
+                                                    `https://${domain}/tx/${tx.txid}`,
+                                                    i
+                                                );
+                                            });
+                                    }).on('error', (e) => {
+                                        console.error(`[ERROR] BLOCKEXPLORER REQUEST 1 FAILED: ${e.message}`);
+                                    });
+
+                                    continue tx;
+                                }
+                            }
+                        }
+
+                    }
+                }
+            });
+    }).on('error', (e) => {
+        console.error(`[ERROR] BLOCKEXPLORER REQUEST 0 FAILED: ${e.message}`);
+    });
+}
+
 /**
  * Handler for Lambda
  */
 exports.handler = function(event, context, callback) {
     // only init slack if set up
+    var slackClient;
     if (process.env.SLACK_WEBHOOK_URL && process.env.SLACK_CHANNEL) {
         const { IncomingWebhook, WebClient } = require('@slack/client');
-        const slackClient = new IncomingWebhook(
+        slackClient = new IncomingWebhook(
             process.env.SLACK_WEBHOOK_URL,
             {channel: process.env.SLACK_CHANNEL}
         );
-    } else {
-        const slackClient = null;
     }
 
     // only init discord if set up
+    var discordClient
     if (process.env.DISCORD_ID && process.env.DISCORD_TOKEN) {
         const Discord = require('discord.js');
-        const discordClient = new Discord.WebhookClient(process.env.DISCORD_ID, process.env.DISCORD_TOKEN);
-    } else {
-        const discordClient = null;
+        discordClient = new Discord.WebhookClient(process.env.DISCORD_ID, process.env.DISCORD_TOKEN);
     }
 
     // Set up the callback so it can be called in each iteration, helps with the slack nonsense
-    var notify = function(message, iteration) {
+    var notify = function(currency, donation, balance, url, iteration) {
+        var message = `New ${currency} donation of ${donation} ${currency}! ` +
+            `The wallet now has a balance of ${balance} ${currency}.` +
+            `\nView the tx here: ${url}`;
+
+        /* global slackClient */
         if (slackClient) {
             // Delay Slack messages so they appear in order 
             // if we just called `send`, they'd hit Slack in random orders
@@ -103,6 +169,7 @@ exports.handler = function(event, context, callback) {
             }, 500 * iteration);
         }
 
+        /* global discordClient */
         if (discordClient) {
             discordClient.send(message)
                 .then(message => console.log('DISCORD: NOTIFICATION SENT'))
@@ -110,5 +177,8 @@ exports.handler = function(event, context, callback) {
         }
     }
 
-    getDonationsFromCryptoID('ecc', '516674', notify, process.env.CHECK_INTERVAL || 60);
-};
+    var checkInterval = process.env.CHECK_INTERVAL || 60;
+    // getDonationsFromCryptoID('ecc', '516674', notify, checkInterval);
+    // getDonationsFromCryptoID('ltc', '24758298', notify, checkInterval);
+    getDonationsFromBlockExplorer('btc', '1KFHE7w8BhaENAswwryaoccDb6qcT6DbYY', notify, 60);
+}();
